c68dd308c7f7e456397f24fc9b80fe61a29ad7b9
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-01-28 16:53:31 +0200

    task-8/1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,28 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
+const renderGalleryItems = (pictures) =>
+  pictures.reduce(
+    (acc, picture) =>
+      acc +
+      `<a class="gallery__item" href=${picture.original}>
+      <img class="gallery__image" src=${picture.preview} alt=${picture.description} />
+    </a>
+    `,
+    ""
+  );
+
+const insertGalleryItems = (string) => {
+  
+  gallery.insertAdjacentHTML("beforeend", string);
+};
+insertGalleryItems(renderGalleryItems(galleryItems));
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+    scrollZoom: false,
+  });
+
 console.log(galleryItems);
