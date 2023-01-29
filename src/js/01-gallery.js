fa8bf747da899168cb5ca065627475f307cc40fb
Andrii Safonov <105196765+AndriySafonov@users.noreply.github.com>
2023-01-29 21:40:15 +0200

    task-3

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import 'simpleLightbox/dist/simple-lightbox.min.css';
 
 const gallery = document.querySelector(".gallery");
 const renderGalleryItems = (pictures) =>
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,20 @@
-// console.log(localStorage);
 
-// localStorage.setItem('my-data', JSON.stringify({ name: 'Mango, age: 2' }));
-
-// const savedData = localStorage.getItem('my-data');
-// console.log('saveData', savedData);
-
-// const parseData = JSON.parse(savedData);
-// console.log('parseData', parseData);
 import throttle from 'lodash.throttle';
-
+const formEl = document.querySelector('.feedback-form');
 const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {};
 
-const refs = {
-  form: document.querySelector('.feedback-form'),
-  textarea: document.querySelector('.feedback-form textarea'),
-};
-refs.form.addEventListener('submit', onFormSubmit);
+updateForm();
 
-refs.form.addEventListener('input', e => {
+formEl.addEventListener('input', throttle(onFormInput, 500));
+formEl.addEventListener('submit', onFormSubmit);
 
- formData[e.target.name] = e.target.value;
- localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
- 
-});
-
-// populateTextarea();
+function onFormInput(evt) {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+  console.log(formData);
+}
 
 // - Останавливаем поведение по умолчанию
 // - Убираем сообщение из хранилища
@@ -34,37 +22,26 @@ refs.form.addEventListener('input', e => {
 
 function onFormSubmit(evt) {
   evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.target;
 
-  console.log('sell form');
-  evt.currentTarget.reset();
+  if (email.value === '' || message.value === '') {
+    return window.alert('input fields not filled!');
+  }
+  console.log({ Email: email.value, Message: message.value });
+  formEl.reset();
   localStorage.removeItem(STORAGE_KEY);
 }
 
-// - Получаем значение поля
-// - Сохраняем его в хранилище
-// - Можно добавить throttle
 
-// function onInput(evt) {
-//   const email = evt.target.value;
-
-//   localStorage.setItem(STORAGE_KEY, email);
-//   console.log('email', email);
-// }
-
-function onTextareaInput(evt) {
-  const message = evt.target.value;
-
-  localStorage.setItem(STORAGE_KEY, message);
-  console.log('message', message);
-}
-
-// - Получаем значение из хранилища
-// - Если там что то было обновляем дом
-
-function populateTextarea() {
-  const savedMessage = localStorage.getItem(STORAGE_KEY);
-  if (savedMessage) {
-    console.log(savedMessage);
-    refs.textarea.value = savedMessage;
+function updateForm() {
+  if (localStorage.getItem(STORAGE_KEY) === null) {
+    return;
   }
+  const savedForm = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  Object.entries(savedForm).forEach(([name, value]) => {
+    formData[name] = value;
+    formEl.elements[name].value = value;
+  });
 }
